64c9af7b366df75ead03364d9385e036e6750f42
littlesquidcodes <93426088+littlesquidcodes@users.noreply.github.com>
2024-06-10 13:16:15 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+
+# Maria's Weather Web App
+
+My weather web app project, created in the IT School Course.
+
+Based on the OpenWeather API, the app lets you see the weather for the following cities:
+- Bucharest, Romania
+- Timişoara, Romania
+- Oradea, Romania
+- Arad, Romania
+- Sibiu, Romania
+
+
+Live Preview: [Here](https://netlify.com/)
+
---
+++ api/endpoints.js
@@ -0,0 +1,9 @@
+const API_KEY = "f4b355f32c78fc19b06c934cf9278c93";
+
+function getCurrentWeatherEndpoint(city) {
+  return `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ro&units=metric&appid=${API_KEY}`;
+}
+
+function getForecastEndpoint(city) {
+  return `https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=ro&units=metric&appid=${API_KEY}`;
+}
